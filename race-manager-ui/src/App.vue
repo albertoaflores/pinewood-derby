40b34dde3929dbc4a37c23f3865e6df460c8e69b
Alberto Flores <aaflores@gmail.com>
2020-01-21 12:42:17 -0500

    Fixed navbar to use bootstrap menus and Vue router's components

--- race-manager-ui/src/App.vue
+++ race-manager-ui/src/App.vue
@@ -1,25 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/dashboard">Dashboard</router-link>
-    </div>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand to="/">Pinewood Derby</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-navbar-nav>
+              <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+              <b-nav-item to="/racers" disabled>Racers</b-nav-item>
+              <b-nav-item to="/awards" disabled>Awards</b-nav-item>
+              <b-nav-item to="/about">About</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-nav>
+          
+        </b-collapse>
+      </b-navbar>
+    </div>   
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 
-#nav {
-  padding: 30px;
+/* #nav {
+  padding: 20px;
 }
 
 #nav a {
@@ -29,5 +44,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- race-manager-ui/src/views/Home.vue
+++ race-manager-ui/src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Pinewood Derby" src="../assets/pinewood-derby-car-logo.gif">
+    <h1>Welcome to Pinewood Derby</h1>
+    <h4>Race in Progress...</h4>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
