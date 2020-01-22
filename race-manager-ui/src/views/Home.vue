a2c5331b55d8882337bb46d26c9973d2aa046e65
Alberto Flores <aaflores@gmail.com>
2020-01-22 00:24:58 -0500

    Fixed home page so that it's centered

--- race-manager-ui/src/views/Home.vue
+++ race-manager-ui/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Pinewood Derby" src="../assets/pinewood-derby-car-logo.gif">
     <h1>Welcome to Pinewood Derby</h1>
-    <h4>Race in Progress...</h4>
   </div>
 </template>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding-top: 50px;
+}
+</style>
